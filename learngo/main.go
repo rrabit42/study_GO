f1baaa9e05c0258b2437930ef419195bc866f846
rrabit42 <gegiraffe@gmail.com>
2021-02-07 16:35:17 +0900

    [Nomad] Theory: Variables and Constants

--- learngo/main.go
+++ learngo/main.go
@@ -1,16 +1,20 @@
-// 진입점: 컴파일러는 package 이름이 main인 것부터 찾음
-// main은 컴파일을 위해서 필요함. 컴파일이 필요없다면 main.go 없어도 됨.
-// 내가 작성할 pakage 이름
 package main
 
-import (
-	"fmt" // fmt는 formmating을 위한 package
-
-	"github.com/rrabit42/learngo/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world") // GO에서 function을 export하고 싶으면 대문자로 시작해야함
-	something.SayHello()       // 다른 패키지로부터 exported function
-	// something.sayBye() : private function
+	// type이 없는 constant(상수), 값 변경 불가
+	// const name = "nico"
+	// go는 type 언어, 무슨 타입인지 알려줘야함
+	const name string = "nico"
+
+	// variable
+	// var name2 string = "nico"
+	// name2 = "lynn" => 값 변경 가능
+	name2 := "nico"
+	// 같은 코드 축약형, type은 go가 알아서 찾아줌 그래서 type을 임의로 변경 못함
+	// **첫번째 값의 type에 의존**해서 두번째 값의 type이 정해지기 때문
+	// **func 안에서만 축약형 사용 가능. 변수에만 적용 가능**
+
+	fmt.Println(name2)
 }
