99f1eb993d5b1cd9f004246ee0b0cce6370dc956
rrabit42 <gegiraffe@gmail.com>
2021-02-12 04:11:41 +0900

    [Nomad] Theory: Pointers

--- learngo/main.go
+++ learngo/main.go
@@ -2,40 +2,24 @@ package main
 
 import "fmt"
 
-func canIDrint(age int) bool {
-	// 익히 알다시피, 아래와 같이 쓸 수도 있지만
-	// switch age {
-	// case 10:
-	// 	return false
-	// case 18:
-	// 	return true
-	// case 50:
-	// 	return false
-	// }
-
-	// 이렇게 아예 대놓고 if-else 대체로도 쓸 수 있음
-	// switch {
-	// case age < 10:
-	// 	return false
-	// case age == 18:
-	// 	return true
-	// case age > 50:
-	// 	return false
-	// }
-	// return false
-
-	// if-else처럼 variable을 만들 수도 있음
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	case 50:
-		return false
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrint(18))
+	/*
+		a := 2
+		b := a
+		a = 10 // b는 바뀌지 않음
+
+		fmt.Println(a, b)
+	*/
+
+	a := 2
+	b := &a // b는 a의 메모리 주소를 들여다 보고 있음
+
+	fmt.Println(&a, b) // 동일함(a는 그냥 2로 출력됨)
+	fmt.Println(*b)    // *는 loop through, see through의 의미! 즉, b가 들여다 보고 있는 a의 값인 2가 나옴
+
+	a = 5
+	fmt.Println(*b)
+
+	*b = 20 // b를 이용해 a 값을 변화시킴
+	fmt.Println(a)
 }
