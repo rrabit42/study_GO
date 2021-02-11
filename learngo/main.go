a0946b94e1ca5750d268738fd7e042090b61f62a
rrabit42 <gegiraffe@gmail.com>
2021-02-12 03:29:53 +0900

    [Nomad] Theory: for, range, args

--- learngo/main.go
+++ learngo/main.go
@@ -1,25 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-// naked return : return할 variable을 굳이 꼭 명시하지 않아도 됨
-// return type과 함께 변수명도 같이 정의해 주는 것!
-// 무엇이 리턴되는지 정확히 알 수 있음
-func lenAndUpper(name string) (length int, uppercase string) {
-	// return 후에 작업을 시키고 싶을 때는
-	// defer를 쓴다. 'defer'은 function 값이 return하고 나면 실행됨
-	defer fmt.Println("I'm done")
+func superAdd(numbers ...int) int {
+	total := 0
 
-	length = len(name) // update, not create 때문에 :=를 쓰는게 아님
-	uppercase = strings.ToUpper(name)
-	return
-	// return length, uppercase 이렇게 해도 당연히 작동하지만, 굳이 작성하지 않아도 됨
+	// range : array에 loop을 적용할 수 있도록 해줌(for 안에서만 사용 가능)
+	for _, number := range numbers { // range는 index를 주기 때문에 이렇게 index를 첫번째로 받아줘야함(_ 하든지)
+		total += number
+	}
+	return total
+
+	// 당연히 이렇게도 가능!
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
 }
 
 func main() {
-	totalLength, up := lenAndUpper("nico") // 즉 여기 function이 끝나면 defer가 실행됨
-	fmt.Println(totalLength, up)           // 그 후에 출력!
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
