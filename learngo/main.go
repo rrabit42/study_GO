68d227ce374614e6a0be4fcbb7e42940626317cf
rrabit42 <gegiraffe@gmail.com>
2021-02-12 04:18:55 +0900

    [Nomad] Theory: Arrays and Slices

--- learngo/main.go
+++ learngo/main.go
@@ -3,23 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		a := 2
-		b := a
-		a = 10 // b는 바뀌지 않음
+	// array
+	// names := [5]string{"nico", "lynn", "dal"}
+	// names[3] = "alala"
+	// fmt.Println(names)
+	// 이 상태로도 출력은 됨.
 
-		fmt.Println(a, b)
-	*/
-
-	a := 2
-	b := &a // b는 a의 메모리 주소를 들여다 보고 있음
-
-	fmt.Println(&a, b) // 동일함(a는 그냥 2로 출력됨)
-	fmt.Println(*b)    // *는 loop through, see through의 의미! 즉, b가 들여다 보고 있는 a의 값인 2가 나옴
-
-	a = 5
-	fmt.Println(*b)
-
-	*b = 20 // b를 이용해 a 값을 변화시킴
-	fmt.Println(a)
+	// slice : 데이터 타입 중 하나, 기본적으로 array 인데 length 없이 사용하는 것
+	// 요소 추가, 삭제가 자유로움. 추가할 때 array처럼 index로 지정이 불가하니 append(slice, value) 이용
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "flynn") // append는 modify가 아님. 새로운 값이 추가된 slice를 return함
+	fmt.Println(names)
 }
