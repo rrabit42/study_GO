22603a2cb0c56f93f5fc6c87d5fe063d11dfaaaf
rrabit42 <gegiraffe@gmail.com>
2021-02-12 18:04:55 +0900

    [Nomad] Bank: Methods part 2 with error handling

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // 컴파일을 하지 않을 거라 main function을 가지지 않음
 // main을 컴파일 할거고, main은 banking을 사용할 것
 
@@ -12,6 +14,8 @@ type Account struct {
 	owner   string
 	balance int
 }
+
+var errnoMoney = errors.New("Can't withdraw")
 
 // Go의 패턴
 // constructor가 없기 때문에 function으로 construct하거나 struct를 만들도록 한다.
@@ -27,11 +31,29 @@ func NewAccount(owner string) *Account {
 // 보통 method의 receiver를 작성할 때, struct의 첫글자를 따서 소문자로 지음
 
 // Deposit X amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
+	// (a Account)
+	// GO가 여기서 a를 받아올 때 복사본을 만들어서 가져옴(safety를 위해, method가 속해있는 object를 보호하려고)
+	// 즉, 이 프로젝트에서는 account의 복사본, 실제 account가 아니라는 소리
+	// 복사본을 만들지 않고 그대로 account를 가져오고 싶으면 (a *Account): Deposit method를 호출한 account를 사용해라(pointer receiver)
 	a.balance += amount
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		// 에러 발생시키기 -> error.Error(){}
+		// 아래와 같이 새로운 에러를 만들 수도 있음
+		return errnoMoney
+	}
+	a.balance -= amount
+
+	// error를 return 시켰으니 정상이어도 뭔가를 return해줘야함
+	// error도 2가지 value가 있는데 하나는 error, 하나는 nil(null, None 같은 것)
+	return nil
 }
--- main.go
+++ main.go
@@ -10,4 +10,13 @@ func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+
+	// 고는 이렇게 항상 에러를 체크하도록 강제시킴(exception이 없으니까)
+	if err != nil {
+		// Println()을 호출하고 프로그램을 종료시킴
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
