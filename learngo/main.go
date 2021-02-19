423d50adbe5ce725fadda762b21e2271cd9cd184
rrabit42 <gegiraffe@gmail.com>
2021-02-20 03:20:26 +0900

    [Nomad] URL Checker: Slow ver

--- learngo/main.go
+++ learngo/main.go
@@ -9,6 +9,14 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	// panic은 컴파일러가 못찾아낸 error
+	// 초기화된 map에 값을 추가할 수 없음 -> map이 nil이 되어버리기 때문
+	// 따라서 빈 map을 선언하고 싶으면 뒤에 {} 빈 중괄호 넣어주기
+	// var results = map[string]string{}
+	// 혹은 make() : map을 만들어주는 func 사용, emtpy map을 초기화하고 싶을 때
+
+	var results = make(map[string]string)
+
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,7 +29,16 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	// fmt.Println(results) 하면 map[key:value, key:value...] 이런식으로 나옴
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -33,7 +50,8 @@ func hitURL(url string) error {
 
 	// request 보내고, response와 error 받기
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 { // 에러가 있다면!
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
