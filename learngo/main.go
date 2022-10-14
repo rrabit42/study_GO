b3160673b48afd25d5096b6daf96ef56a06d2505
rrabit42 <gegiraffe@gmail.com>
2022-10-14 11:31:17 +0900

    [Nomad] Fast URLChecker

--- learngo/main.go
+++ learngo/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
-var errRequestFailed = errors.New("Request failed")
+type requestResult struct {
+	url    string
+	status string
+}
 
 func main() {
-	// var results map[string]string 이렇게 선언하면 panic 발생
-	// panic은 컴파일러가 못찾아낸 error
-	// 초기화되지 않은 map에 값을 추가할 수 없음 -> map이 nil이 되어버리기 때문
-	// 따라서 빈 map을 선언하고 싶으면 뒤에 {} 빈 중괄호 넣어주기
-	// var results = map[string]string{}
-	// 혹은 make() : map을 만들어주는 func 사용, emtpy map을 초기화하고 싶을 때
-
-	var results = make(map[string]string)
-
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -30,30 +25,25 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	// fmt.Println(results) 하면 map[key:value, key:value...] 이런식으로 나옴
-	for url, result := range results {
-		fmt.Println(url, result)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-// 웹사이트 접속
-// go lang std library 이용
-func hitURL(url string) error {
-	// logger
-	fmt.Println("Checking:", url)
-
-	// request 보내고, response와 error 받기
+// chan<- ; 이 채널은 Send Only
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 { // 에러가 있다면!
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
